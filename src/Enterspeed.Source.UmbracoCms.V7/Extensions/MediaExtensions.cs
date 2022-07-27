b2e66b554db528614250e6724e0bc3b7a5910f91
Mathias Hove <mathiashove02@gmail.com>
2022-07-27 16:02:59 +0200

    Simplifying method

--- src/Enterspeed.Source.UmbracoCms.V7/Extensions/MediaExtensions.cs
+++ src/Enterspeed.Source.UmbracoCms.V7/Extensions/MediaExtensions.cs
@@ -10,24 +10,16 @@ namespace Enterspeed.Source.UmbracoCms.V7.Extensions
     {
         public static string GetMediaUrl(this IMedia media, EnterspeedUmbracoConfiguration configuration)
         {
-            string url = string.Empty;
             var umbracoFile = media.GetValue<string>(Constants.Conventions.Media.File);
             if (umbracoFile != null && umbracoFile.Contains("src"))
             {
                 var umbFile = JsonConvert.DeserializeObject<UmbFile>(umbracoFile);
-                if (umbFile != null)
-                {
-                    url = configuration.MediaDomain + umbFile.Src;
-                }
-            }
-            else
-            {
-                // Should be a complex type, but is sometimes only a string. I don't know why.
-                // Might be some behaviour in Umbraco
-                url = umbracoFile;
+                return umbFile != null ? configuration.MediaDomain + umbFile.Src : string.Empty;
             }
 
-            return url;
+            // Should be a complex type, but is sometimes only a string. I don't know why.
+            // Might be some behaviour in Umbraco
+            return umbracoFile;
         }
     }
 }
