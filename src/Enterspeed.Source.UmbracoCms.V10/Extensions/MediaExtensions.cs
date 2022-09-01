2e29116284a6b52052e6f1bc35e2c48ffb769d29
Mathias Hove <mathiashove02@gmail.com>
2022-09-01 12:39:18 +0200

    Implemented job handlers and changes for a few service, to handle media in V10

---
+++ src/Enterspeed.Source.UmbracoCms.V10/Extensions/MediaExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Enterspeed.Source.UmbracoCms.V10.Models.Configuration;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+
+namespace Enterspeed.Source.UmbracoCms.V10.Extensions
+{
+    public static class MediaExtensions
+    {
+        public static string GetMediaUrl(this IMedia media, EnterspeedUmbracoConfiguration configuration)
+        {
+            var umbracoFile = media.GetValue<string>(Constants.Conventions.Media.File);
+            var url = string.Empty;
+
+            if (umbracoFile != null)
+            {
+                url = configuration.MediaDomain + JsonConvert.DeserializeObject<ImageCropperValue>(umbracoFile)?.Src;
+            }
+
+            return url;
+        }
+    }
+}
