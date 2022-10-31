c527c724c04ea0c9b4bf505a968accd83a514ceb
jesperweber <jlw@enterspeed.com>
2022-10-31 08:20:34 +0100

    Fixed Umbraco install error when Enterspeed plugin is installed (Umbraco 9 & 10) (#69)

--- src/Enterspeed.Source.UmbracoCms.V9/Components/EnterspeedJobsComponent.cs
+++ src/Enterspeed.Source.UmbracoCms.V9/Components/EnterspeedJobsComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Enterspeed.Source.UmbracoCms.V9.Data.Migration;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Migrations;
 using Umbraco.Cms.Core.Scoping;
@@ -13,19 +14,25 @@ namespace Enterspeed.Source.UmbracoCms.V9.Components
         private readonly IScopeProvider _scopeProvider;
         private readonly IKeyValueService _keyValueService;
         private readonly IMigrationPlanExecutor _migrationPlanExecutor;
+        private readonly IRuntimeState _runtimeState;
 
         public EnterspeedJobsComponent(
             IScopeProvider scopeProvider,
             IKeyValueService keyValueService,
-            IMigrationPlanExecutor migrationPlanExecutor)
+            IMigrationPlanExecutor migrationPlanExecutor,
+            IRuntimeState runtimeState)
         {
             _scopeProvider = scopeProvider;
             _keyValueService = keyValueService;
             _migrationPlanExecutor = migrationPlanExecutor;
+            _runtimeState = runtimeState;
         }
 
         public void Initialize()
         {
+            if (_runtimeState.Level < RuntimeLevel.Run)
+                return;
+
             var migrationPlan = new MigrationPlan("EnterspeedJobs");
             migrationPlan.From(string.Empty)
                 .To<EnterspeedJobsTableMigration>("enterspeedjobs-db")
