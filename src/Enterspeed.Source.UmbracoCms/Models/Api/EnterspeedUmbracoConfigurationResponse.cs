30f28fe066638138919546adc8cf8c14a29980e4
jesperweber <jlw@enterspeed.com>
2023-02-08 09:46:03 +0100

    Notify users if Umbraco serverRole is not configured correct (#86)

---
+++ src/Enterspeed.Source.UmbracoCms/Models/Api/EnterspeedUmbracoConfigurationResponse.cs
@@ -0,0 +1,17 @@
+﻿using Enterspeed.Source.UmbracoCms.Models.Configuration;
+using Umbraco.Cms.Core.Sync;
+
+namespace Enterspeed.Source.UmbracoCms.Models.Api
+{
+    public class EnterspeedUmbracoConfigurationResponse : EnterspeedUmbracoConfiguration
+    {
+        public ServerRole ServerRole { get; }
+        public EnterspeedUmbracoConfiguration Configuration { get; }
+
+        public EnterspeedUmbracoConfigurationResponse(EnterspeedUmbracoConfiguration enterspeedUmbracoConfiguration, ServerRole serverRole)
+        {
+            Configuration = enterspeedUmbracoConfiguration;
+            ServerRole = serverRole;
+        }
+    }
+}
