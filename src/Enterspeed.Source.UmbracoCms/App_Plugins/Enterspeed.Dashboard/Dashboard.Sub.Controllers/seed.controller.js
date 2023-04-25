5bd9d23c26527152161270f1d82dac9ab837cf7c
jesperweber <jweber@jweber.dk>
2023-04-25 15:24:54 +0200

    Custom seed - first version

--- src/Enterspeed.Source.UmbracoCms/App_Plugins/Enterspeed.Dashboard/Dashboard.Sub.Controllers/seed.controller.js
+++ src/Enterspeed.Source.UmbracoCms/App_Plugins/Enterspeed.Dashboard/Dashboard.Sub.Controllers/seed.controller.js
@@ -1,16 +1,23 @@
-﻿function seedController(dashboardResources, notificationsService) {
+﻿function seedController(dashboardResources, notificationsService, editorService) {
     var vm = this;
     vm.seedState = "success";
+    vm.seedModes = ['Everything', 'Custom'];
+    vm.selectedSeedMode = vm.seedModes[0];
+    vm.seedModeSelectOpen = false;
     vm.seedResponse = null;
-
-    function init() {
-    }
+    vm.contentTree = [];
+    vm.mediaTree = [];
+    vm.dictionaryTree = [];
+    vm.selectedNodesToSeed = [];
+    vm.selectedNodesToSeed['Content'] = [];
+    vm.selectedNodesToSeed['Media'] = [];
+    vm.selectedNodesToSeed['Dictionary'] = [];
 
     vm.seed = function () {
         vm.seedState = "busy";
         dashboardResources.seed().then(function (result) {
             if (result.data.isSuccess) {
-                notificationsService.success("Seed", "Successfully started seeding Enterspeed")
+                notificationsService.success("Seed", "Successfully started seeding Enterspeed");
                 vm.seedResponse = result.data.data;
             } else {
                 vm.seedResponse = null;
@@ -21,7 +28,41 @@
         });
     };
 
-    init();
+    vm.selectNode = function() {
+        editorService.open({
+            title: "Select node",
+            subtitle: "Select a node to include in the seed",
+            view: "/App_Plugins/Enterspeed.Dashboard/Dashboard.Sub.Views/selectNode.html",
+            size: "small",
+            submit: function(value) {
+                if (!value.target || !value.target.id) {
+                    return;
+                }
+
+                var existingNodeIndex = vm.selectedNodesToSeed[value.target.contentType].findIndex(element => element.id === value.target.id);
+                if (existingNodeIndex >= 0) {
+                    vm.selectedNodesToSeed[value.target.contentType][existingNodeIndex] = value.target;
+                } else {
+                    vm.selectedNodesToSeed[value.target.contentType].push(value.target);
+                }
+            },
+            close: function() {
+                editorService.close();
+            }
+        });
+    }
+
+    vm.removeSelectNode = function(section, index) {
+        vm.selectedNodesToSeed[section].splice(index, 1);
+    }
+
+    vm.setSeedMode = function (seedMode) {
+        vm.selectedSeedMode = seedMode;
+    }
+
+    vm.toggleSeedModeSelect = function () {
+        vm.seedModeSelectOpen = !vm.seedModeSelectOpen;
+    }
 }
 
 angular.module("umbraco").controller("SeedController", seedController);
---
+++ src/Enterspeed.Source.UmbracoCms/App_Plugins/Enterspeed.Dashboard/Dashboard.Sub.Controllers/selectNode.controller.js
@@ -0,0 +1,72 @@
+﻿function selectNodeController($scope) {
+    var vm = this;
+    vm.contentTypes = ['Content', 'Media', 'Dictionary'];
+    vm.contentTypeSelectOpen = false;
+    vm.includeDescendents = false;
+    vm.selectedContentType = vm.contentTypes[0];
+
+    function init() {
+        resetNodeSelection();
+    }
+
+    $scope.onTreeInit = function () {
+        $scope.dialogTreeApi.callbacks.treeNodeSelect(nodeSelectHandler);
+    }
+
+    function resetNodeSelection() {
+        $scope.dialogTreeApi = {};
+        $scope.model.target = {};
+    }
+
+    function nodeSelectHandler(args) {
+        if (args && args.event) {
+            args.event.preventDefault();
+            args.event.stopPropagation();
+        }
+
+        if ($scope.currentNode) {
+            //un-select if there's a current one selected
+            $scope.currentNode.selected = false;
+        }
+
+        $scope.currentNode = args.node;
+        $scope.currentNode.selected = true;
+        $scope.model.target.id = args.node.id;
+        $scope.model.target.udi = args.node.udi;
+        $scope.model.target.name = args.node.name;
+        $scope.model.target.icon = args.node.icon;
+        $scope.model.target.contentType = vm.selectedContentType;
+    }
+
+    vm.setContentType = function (contentType) {
+        vm.selectedContentType = contentType;
+        resetNodeSelection();
+    }
+
+    vm.toggleContentTypeSelect = function () {
+        vm.contentTypeSelectOpen = !vm.contentTypeSelectOpen;
+    }
+
+    vm.toggleIncludeDescendents = function () {
+        vm.includeDescendents = !vm.includeDescendents;
+    }
+
+    vm.close = function () {
+        if ($scope.model && $scope.model.close) {
+            $scope.model.close();
+        }
+    }
+
+    vm.submit = function () {
+        if ($scope.model && $scope.model.submit) {
+            $scope.model.target.includeDescendents = vm.includeDescendents;
+            $scope.model.submit($scope.model);
+        }
+
+        vm.close();
+    }
+
+    init();
+}
+
+angular.module("umbraco").controller("SelectNodeController", selectNodeController);
