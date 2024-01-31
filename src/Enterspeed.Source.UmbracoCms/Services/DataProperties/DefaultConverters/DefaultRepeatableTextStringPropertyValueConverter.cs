6a5a0c06d7c948cf5d56b5023a32e95feb10f0d8
jesperweber <jlw@enterspeed.com>
2024-01-31 10:43:16 +0100

    Fixed error for empty repeatable string (#146)

--- src/Enterspeed.Source.UmbracoCms/Services/DataProperties/DefaultConverters/DefaultRepeatableTextStringPropertyValueConverter.cs
+++ src/Enterspeed.Source.UmbracoCms/Services/DataProperties/DefaultConverters/DefaultRepeatableTextStringPropertyValueConverter.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Enterspeed.Source.Sdk.Api.Models.Properties;
 using Enterspeed.Source.UmbracoCms.Extensions;
-using Enterspeed.Source.UmbracoCms.Services.DataProperties;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace Enterspeed.Source.UmbracoCms.Services.DataProperties.DefaultConverters
@@ -15,7 +14,7 @@ namespace Enterspeed.Source.UmbracoCms.Services.DataProperties.DefaultConverters
 
         public virtual IEnterspeedProperty Convert(IPublishedProperty property, string culture)
         {
-            var items = property.GetValue<string[]>(culture);
+            var items = property.GetValue<IEnumerable<string>>(culture);
             var arrayItems = new List<IEnterspeedProperty>();
             foreach (var item in items)
             {
