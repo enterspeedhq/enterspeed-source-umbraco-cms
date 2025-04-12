84d4b12445643bbb4d7e0754b626714950600a2b
MHove89 <mathiashove02@gmail.com>
2025-04-12 22:29:56 +0200

    Fix bug with context being null, when blocks are rendered in RTE as background job

--- src/Enterspeed.Source.UmbracoCms.Base/Services/DataProperties/DefaultConverters/DefaultRichTextEditorPropertyValueConverter.cs
+++ src/Enterspeed.Source.UmbracoCms.Base/Services/DataProperties/DefaultConverters/DefaultRichTextEditorPropertyValueConverter.cs
@@ -1,18 +1,32 @@
 ﻿using Enterspeed.Source.Sdk.Api.Models.Properties;
 using Enterspeed.Source.UmbracoCms.Base.Extensions;
+using Enterspeed.Source.UmbracoCms.Base.Providers;
+using Microsoft.AspNetCore.Http;
 using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
 
 namespace Enterspeed.Source.UmbracoCms.Base.Services.DataProperties.DefaultConverters
 {
     public class DefaultRichTextEditorPropertyValueConverter : IEnterspeedPropertyValueConverter
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUmbracoContextFactory _umbracoContextAccessor;
         private readonly IUmbracoRichTextParser _umbracoRichTextParser;
         private readonly IEnterspeedConfigurationService _enterspeedConfigurationService;
+        private readonly IEnterspeedHttpContextProvider _enterspeedHttpContextProvider;
 
-        public DefaultRichTextEditorPropertyValueConverter(IUmbracoRichTextParser umbracoRichTextParser, IEnterspeedConfigurationService enterspeedConfigurationService)
+        public DefaultRichTextEditorPropertyValueConverter(
+           IHttpContextAccessor httpContextAccessor,
+           IUmbracoContextFactory umbracoContextAccessor,
+           IUmbracoRichTextParser umbracoRichTextParser,
+           IEnterspeedConfigurationService enterspeedConfigurationService,
+           IEnterspeedHttpContextProvider enterspeedHttpContextProvider)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _umbracoContextAccessor = umbracoContextAccessor;
             _umbracoRichTextParser = umbracoRichTextParser;
             _enterspeedConfigurationService = enterspeedConfigurationService;
+            _enterspeedHttpContextProvider = enterspeedHttpContextProvider;
         }
 
         public bool IsConverter(IPublishedPropertyType propertyType)
@@ -21,6 +35,22 @@ namespace Enterspeed.Source.UmbracoCms.Base.Services.DataProperties.DefaultConve
         }
 
         public virtual IEnterspeedProperty Convert(IPublishedProperty property, string culture)
+        {
+            if (_httpContextAccessor.HttpContext == null)
+            {
+                using (_enterspeedHttpContextProvider.CreateFakeHttpContext())
+                {
+                    using (_umbracoContextAccessor.EnsureUmbracoContext())
+                    {
+                        return ConvertWithContext(property, culture);
+                    }
+                }
+            }
+
+            return ConvertWithContext(property, culture);
+        }
+
+        private IEnterspeedProperty ConvertWithContext(IPublishedProperty property, string culture)
         {
             var value = property.GetValue<Umbraco.Cms.Core.Strings.HtmlEncodedString>(culture).ToString();
             value = _umbracoRichTextParser.PrefixRelativeImagesWithDomain(value, _enterspeedConfigurationService.GetConfiguration().MediaDomain);
